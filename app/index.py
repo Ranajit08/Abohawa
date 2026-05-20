0b5a29311e090a1cf87a5bd9f821ddd1a62ea199
Ranajit Mitra <creativeranajit12@gmail.com>
2026-05-20 20:46:22 +0530

    removed some parts

--- app/index.py
+++ app/index.py
@@ -10,11 +10,4 @@ SECRET_KEY = os.getenv('API_KEY')
 
 @bp.route('/')
 def index():
-    a = requests.get(f'http://api.weatherapi.com/v1/current.json?key={SECRET_KEY}&q=22.6855752,88.3748587') 
     return render_template('index.html')
-
-@bp.route('/search/<t>')
-def search(t):
-    a = requests.get(f'https://photon.komoot.io/api/?q={t}')
-    return a.json()
-
